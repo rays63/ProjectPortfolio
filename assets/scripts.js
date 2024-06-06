469addbbd4b4cbc97f1247aa92e9d4a4cf58e605
Raymz <raymondmhz63@gmail.com>
2024-06-06 16:50:33 +0545

    initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,16 @@
+function expandCard(card) {
+    const modal = $('#projectModal');
+    const title = $(card).find('.card-title').text();
+    const imageSrc = $(card).find('img').attr('src');
+    const description = "This is a detailed description of " + title;
+    const languages = $(card).find('.card-text').text();
+    const githubLink = "https://github.com/your-repo"; // Replace with actual link
+
+    modal.find('.modal-title').text(title);
+    modal.find('#modalImage').attr('src', imageSrc);
+    modal.find('#modalDescription').text(description);
+    modal.find('#modalLanguages').text(languages);
+    modal.find('#modalGithubLink').attr('href', githubLink);
+
+    modal.modal('show');
+}
